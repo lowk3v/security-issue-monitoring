60d17ec23db7b17aea1bd22ef65335e7d87f1eb8
Kev <davidkevinit@gmail.com>
2020-03-02 23:04:28 +0000

    add more providers

--- setting-example.py
+++ setting-example.py
@@ -1,7 +1,19 @@
-
 __providers_newsfeed__ = {
+    'DoYenSec': 'https://blog.doyensec.com/atom.xml',
+    'Mogwailabs': 'https://mogwailabs.de/blog/index.xml',
+    'KnownSec 404 team': 'https://medium.com/feed/@knownsec404team',
+    'SeeBug': 'https://www.seebug.org/rss/new/',
+    'SeeBug paper': 'https://paper.seebug.org/rss/',
+    'Codesec Bsmali4': 'http://www.codersec.net/feed.xml',
+    'Trend Micro': 'https://blog.trendmicro.com/feed/',
+    'Rips Tech': 'https://blog.ripstech.com/index.xml',
+    'Project Zero': 'https://googleprojectzero.blogspot.com/feeds/posts/default?alt=rss',
     'NccGroup': 'https://www.nccgroup.trust/us/about-us/newsroom-and-events/blog/us-blog-rss-feed/',
-    'Code White Sec': 'https://codewhitesec.blogspot.com/feeds/posts/default?alt=rss'
+    'Code White Sec': 'https://codewhitesec.blogspot.com/feeds/posts/default?alt=rss',
+#    'ZDNet': 'https://www.zdnet.com/news/rss.xml',
+    'Zero Day Initiative': 'https://www.thezdi.com/blog?format=rss',
+    'PortSwigger RD': 'https://portswigger.net/research/rss',
+    'PortSwigger blog': 'https://portswigger.net/blog/rss',
 }
 
 __providers_issue__ = {}
